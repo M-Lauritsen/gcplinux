12d1c24288e23aac3f50e2a0498148fb4c0d00c9
Michael <i30288741@gmail.com>
2020-06-26 08:04:00 +0200

    unit testing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <title>Unit Testing</title>
+        <p>Unit Testing</p>
         <a
           className="App-link"
           href="https://reactjs.org"
