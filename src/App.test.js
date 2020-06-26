7c15ce18a52a8e2e672fee297c0c5b28eacee796
Michael <i30288741@gmail.com>
2020-06-26 07:56:42 +0200

    unit testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
+
+describe("Testing App.js", () => {
+  test("Checking if class exists", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("div").hasClass("App"));
+  });
+  test("Checking if the title is correct", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("title").text()).toContain("Unit Testing");
+  });
 });
